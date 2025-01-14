8becc27254d06c12bafcb8f0bc3a4c56c3f4e6c5
AMITKUMAR7970 <110619807+AMITKUMAR7970@users.noreply.github.com>
2025-01-14 21:10:34 +0530

    Add files via upload

---
+++ build/CMakeFiles/OrderManagementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OrderManagementSystem.dir/src/main.cpp.obj"
+  "CMakeFiles/OrderManagementSystem.dir/src/main.cpp.obj.d"
+  "OrderManagementSystem.exe"
+  "OrderManagementSystem.exe.manifest"
+  "OrderManagementSystem.pdb"
+  "libOrderManagementSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OrderManagementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
